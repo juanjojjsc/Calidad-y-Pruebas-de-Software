a71a27647eb43e122baf804bc08f6d42d80551ea
JJ Santos <jj@HAL-9000.local>
2017-08-15 13:21:14 -0500

    Circuales Primos con TEST

--- unitTest/CircularPrime/CircularPrime/testPrimos.cc
+++ unitTest/CircularPrime/CircularPrime/testPrimos.cc
@@ -3,11 +3,27 @@
 
 TEST(TestCaseName, Positivos)
 {
-	while(1==1)
+
+long int i=2;
+  while(i<=10000)
   {
-    for (int i =0; i<20; i++)
-    {
-      EXPECT_TRUE(isPrime(i));
-    }
+      fl=0;
+      rotate(i);
+      if(fl==0)
+      {
+          cout<<"\n"<<i;
+          EXPECT_TRUE(true);
+      }
+      i++;
   }
+
+
+	// while(1==1)
+  // {
+  //   for (int i =0; i<20; i++)
+  //   {
+  //
+  //     EXPECT_TRUE(isPrime(i));
+  //   }
+  // }
 }
